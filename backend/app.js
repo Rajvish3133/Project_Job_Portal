ebc50e88eadd431cc74daea43888d993944eff75
Raj Vishwakarma <rv2477207@gmail.com>
2026-01-09 01:51:02 +0530

    change in backend url

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ config({path : "./config/config.env"});
 app.use(cors({
      origin: [
     "http://localhost:5173",
-    "http://localhost:5174",
+    "https://project-job-portal-frontend.onrender.com/",
   ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
